7b51215c09248708e8edeb6a4a721f4594711f01
Anand-1912 <34396688+Anand-1912@users.noreply.github.com>
2024-03-04 16:30:44 +0000

    add AsParallel to create a Parallel Query

--- ParallelLINQ/Program.cs
+++ ParallelLINQ/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+
+sw.Start();
+
+var collection = Enumerable.Range(1, 100)
+    .AsParallel()
+    .Select(HeavyCompuation);
+
+int HeavyCompuation(int n)
+{
+    for (int i = 0; i < 100_000_000; i++) n += i;
+    return n;
+}
+
+foreach (var _ in collection) ;
+
+sw.Stop();
+
+Console.WriteLine($"Execution time {sw.ElapsedMilliseconds}");
+
+Console.ReadLine();
